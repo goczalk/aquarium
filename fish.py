7bb69d27dffe80236c4afde55a28ff4ce5ebb23c
Klaudia Goczal <klaudia@lodz.pl>
2016-11-06 21:56:14 +0100

    Initial graphic movement of the fish [circle]. Create oval and move it in random direction

--- fish.py
+++ fish.py
@@ -3,24 +3,31 @@ File with classes of creatures
 """
 
 import random
+from Tkinter import *
 
 class Fish:
-    def __init__(self):
-        self.length = 10
-        self.x = random.randrange(200)
-        self.y = random.randrange(200)
+    def __init__(self, can):
+        self.radius = 10
+        self.x = random.randrange(1000) - self.radius
+        self.y = random.randrange(500) - self.radius
+        self.canvas = can
+
+        self.id = Canvas.create_oval(self.canvas, self.x, self.y, self.x + self.radius, self.y + self.radius)
+
         self.vx = random.randrange(-5, 5)
         self.vy = random.randrange(-5, 5)
 
-    def move(self, t):
-        self.x += self.vx * t
-        self.y += self.vy * t
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.canvas.move(self.id, self.vx, self.vy)
+        self.canvas.after(1, self.move)
 
-    def accelarate(self):
-        self.vx = random.randrange(-5, 5)
-        self.vy = random.randrange(-5, 5)
-        print 'accel:'
-        print self.vx, self.vy
-
-    def print_pos(self):
-        print self.x, self.y
+    # def accelarate(self):
+    #     self.vx = random.randrange(-5, 5)
+    #     self.vy = random.randrange(-5, 5)
+    #     print 'accel:'
+    #     print self.vx, self.vy
+    #
+    # def print_pos(self):
+    #     print self.x, self.y
--- render.py
+++ render.py
@@ -2,19 +2,63 @@
 File with functions to render and update aquarium state
 """
 
-import fish
+from fish import Fish
+from Tkinter import *
 
 
-#test for moving fish
-a = fish.Fish()
+tk = Tk()
+canvas = Canvas(tk, width=1000, height=500, bd=0)
 
-a.print_pos()
-i=0
-while (i != 10):
-    print 'i = ' + str(i)
-    a.move(1)
-    a.print_pos()
+#pack method tells Tk to fit the size of the window to the given text.
+canvas.pack()
 
-    if ( i % 3  == 0):
-        a.accelarate()
-    i +=1
+fish = Fish(canvas)
+fish.move()
+
+tk.mainloop()
+
+# CREATE APP VIA CLASS
+
+# from Tkinter import *
+#
+# class App(Frame):
+#     def __init__(self, master=None):
+#         Frame.__init__(self, master)
+#         self.pack()
+#         self.canvas = Canvas(master, )
+# # create the application
+# myapp = App(root)
+#
+# #
+# # here are method calls to the window manager class
+# #
+# myapp.master.title("My Do-Nothing Application")
+# myapp.master.maxsize(1000, 400)
+#
+# # start the program
+# myapp.mainloop()
+
+# app = Application(master=root)
+# DIFFERENT WAY TO CREATE APP
+# class Application(Frame):
+#     def createWidgets(self):
+#         self.QUIT = Button(self)
+#         self.QUIT["text"] = "QUIT"
+#         self.QUIT["fg"]   = "red"
+#         self.QUIT["command"] =  self.quit
+#         self.QUIT.pack({"side": "left"})
+#
+#         self.canvas = Canvas(self)
+#         self.canvas["width"] = 1000
+#         self.canvas["height"] = 500
+#         self.canvas["bg"] = "white"
+#         self.canvas.pack()
+#
+#     def createFish(self):
+#         a = Fish(self.canvas)
+#
+#     def __init__(self, master=None):
+#         Frame.__init__(self, master)
+#         self.pack()
+#         self.createWidgets()
+#         self.createFish()
